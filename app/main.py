53070cb883d3424cc127f163c447391c29a277d8
Aditya Rawat <108173854+AdityaUK01@users.noreply.github.com>
2026-07-01 20:32:11 +0530

    update main.py for Render

--- app/main.py
+++ app/main.py
@@ -3,29 +3,44 @@ from fastapi import FastAPI
 from app.catalog import SHLCatalog
 from app.retriever import SHLRetriever
 from app.agent import SHLAgent
-
 from app.schemas import ChatRequest
-
 
 app = FastAPI(
     title="SHL Assessment Recommender"
 )
 
-catalog = SHLCatalog(
-    "data/shl_product_catalog_fixed.json"
-)
+catalog = None
+retriever = None
+agent = None
 
-retriever = SHLRetriever(
-    catalog.get_all()
-)
 
-agent = SHLAgent(
-    retriever
-)
+def get_agent():
+    global catalog, retriever, agent
+
+    if agent is None:
+
+        print("Initializing SHL Agent...")
+
+        catalog = SHLCatalog(
+            "data/shl_product_catalog_fixed.json"
+        )
+
+        retriever = SHLRetriever(
+            catalog.get_all()
+        )
+
+        agent = SHLAgent(
+            retriever
+        )
+
+        print("SHL Agent Ready!")
+
+    return agent
 
 
 @app.get("/health")
 def health():
+
     return {
         "status": "ok"
     }
@@ -33,6 +48,8 @@ def health():
 
 @app.post("/chat")
 def chat(request: ChatRequest):
+
+    agent = get_agent()
 
     response = agent.chat(
         [m.model_dump() for m in request.messages]
